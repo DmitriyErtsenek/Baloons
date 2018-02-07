7004149063f659b67b431dc632a343a59f326b75
DmitriyErtsenek <SolBadguy21@yandex.ru>
2018-02-07 16:39:49 +0300

    Валидация форм. Переверстка БЭМ.

--- app/js/common.js
+++ app/js/common.js
@@ -59,20 +59,60 @@ ymaps.ready(init);
 };
 
 
-  var mainNavClosed = document.querySelector('.main-nav__closed');
-  var mainNav = document.querySelector('.main-nav');
-  var toggle = document.querySelector('.main-nav__btn');
+var mainNavClosed = document.querySelector('.main-nav__closed');
+var mainNav = document.querySelector('.main-nav');
+var toggle = document.querySelector('.main-nav__btn');
 
-  mainNav.classList.remove('main-nav--noJs');
+mainNav.classList.remove('main-nav--noJs');
+mainNav.classList.add('main-nav--closed');
+
+toggle.addEventListener('click', function(){
+ if (mainNav.classList.contains('main-nav--closed')) {
+  mainNav.classList.remove('main-nav--closed');
+ } else {
   mainNav.classList.add('main-nav--closed');
+ }
+});
 
-  toggle.addEventListener('click', function(){
-   if (mainNav.classList.contains('main-nav--closed')) {
-    mainNav.classList.remove('main-nav--closed');
-   } else {
-    mainNav.classList.add('main-nav--closed');
-   }
+$(document).ready(function() {
+  $(function() {
+    $('#orderForm').validate({
+      rules: {
+        userSurname: {
+          required: true
+        },
+        userName: {
+          required: true
+        },
+        userTel: {
+          required: true,
+          digits: true
+        },
+        userEmail: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        userSurname: {
+          required: 'Поле <Фамилия> обязательное для заполнения'
+        },
+        userName: {
+          required: 'Поле <Имя> обязательное для заполнения'
+        },
+        userTel: {
+          required: 'Поле <Телефон> обязательное для заполнения',
+          digits: 'Пожалуйста, введите корректный номер телефона'
+        },
+        userEmail: {
+          required: 'Поле <Адрес почты> обязательное для заполнения',
+          email: 'Пожалуйста, введите корректный адрес почты'
+        }
+      }
+    });
   });
+});
+
 
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,8 @@ gulp.task('symbols', function() {
 gulp.task('scripts', function() {
   return gulp.src([
     'app/libs/jquery/dist/jquery.min.js',
-    'app/libs/owl.carousel/dist/owl.carousel.min.js'
+    'app/libs/owl.carousel/dist/owl.carousel.min.js',
+    'app/libs/jquery-validation/dist/jquery.validate.min.js'
   ])
   .pipe(concat('libs.min.js'))
   .pipe(uglify())
